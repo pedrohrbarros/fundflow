8230a67b5b0de5cecd482f7cb3388eb58e96ed11
Pedro Barros <pedro.barros@publicinterestco.com>
2026-04-30 12:19:20 -0300

    refactor: remove test_endpoint route, service, and tests

--- src/routes/v1/test_endpoint.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from 'elysia'
-import { getTestEndpoint } from './test_endpoint/get'
-
-export const test_endpoint = new Elysia().get('/test_endpoint', getTestEndpoint, {
-  detail: {
-    security: [{ bearerAuth: [] }],
-  },
-})
--- src/services/test_endpoint.ts
+++
@@ -1,9 +0,0 @@
-import { logger } from '../config/logging'
-import type { ServiceResult } from './types'
-
-export const TestEndpointService = {
-  logUserId(user_id: string): ServiceResult<{ user_id: string }> {
-    logger.info({ user_id }, 'Test endpoint called by user')
-    return { ok: true, data: { user_id } }
-  },
-}
